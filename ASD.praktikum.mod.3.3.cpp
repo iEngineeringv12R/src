48da2a0afa423a9b45725669ef012b0b0fd5dca6
blacklizt <black.eu7@gmail.com>
2013-03-10 03:35:43 +0700

    Update ASD.praktikum.mod.3.3.cpp

--- ASD.praktikum.mod.3.3.cpp
+++ ASD.praktikum.mod.3.3.cpp
@@ -1,4 +1,36 @@
 /*
- *
- * UNFINISHED TASK. NEED HELP HERE !
- */
+Copyright 2013 Alfian Gautama Herman (email : fian_gautama@yahoo.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fibbonaci(int n){
+  return (n <= 1) ? n : fibbonaci(n-1) + fibbonaci(n-2);
+}
+
+main(){
+	int input;
+	cout << "Aplikasi kalkulasi fibbonaci\n";
+	cout << "Masukkan angka = "; cin >> input;
+	
+	cout << "\nHasilnya adalah = " ;
+	for(int i = 1; i <= input; i++){
+		cout << fibbonaci(i) << " ";
+	}
+	cout << endl;
+}
