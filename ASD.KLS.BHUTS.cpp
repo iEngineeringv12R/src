ee8dbc4b8f7c6623ae75e6a74a27ba79a242389c
Afhamz Auditore <afh4mz@gmail.com>
2013-04-04 10:45:27 +0700

    Update ASD.KLS.BHUTS.cpp

--- ASD.KLS.BHUTS.cpp
+++ ASD.KLS.BHUTS.cpp
@@ -2,10 +2,10 @@
 buatlah function pointer (jumlah parameter)
 utk menyelesaikan masalah deret
 dengan deret asal { 2, 4, 6, 8, 10}
-  				0  1  2  3  4 
+  		    0  1  2  3  4 
 selesaikan oeleh function2
-	1) function 1 : mengubah inde*x ke 0, 2, 4	-> kalikan dirinya sendiri
-	2) function 2 : mengubah inde*x ke 1, 3		-> kalikan dengan 2
+	1) function 1 : mengubah index ke 0, 2, 4	-> kalikan dirinya sendiri
+	2) function 2 : mengubah index ke 1, 3		-> kalikan dengan 2
 hasil deret { 4, 8, 36, 16, 100 }
 */
 
@@ -16,16 +16,16 @@ using namespace std;
 
 void ptr1(int *x){
   	for(int i = 0; i < 3 ; i++){
-			*x = *x * *x;
-			x+=2;
-		}
+		*x = *x * *x;
+		x+=2;
+	}
 }
 
 void ptr2(int *x){
-		for(int i = 0; i < 2 ; i++){
-			*x = *x * 2;
-			x+=2;
-		}
+	for(int i = 0; i < 2 ; i++){
+		*x = *x * 2;
+		x+=2;
+	}
 }
 
 main(){
@@ -34,10 +34,12 @@ int *p;
 
 	p = deret;
 	for(int i = 0; i < 5 ; i++){
-		cout << *p	<<" ";
+		cout << *p <<" ";
 		p++;
 	}
+	
 	cout << endl << endl;
+	
 	p = &deret[0];
 	ptr1(p);
 	
@@ -46,8 +48,9 @@ int *p;
 	
 	p = deret;
 	for(int i = 0; i < 5 ; i++){
-		cout << *p	<<" ";
+		cout << *p <<" ";
 		p++;
-	}	
+	}
+	
 	cout << endl << endl;	
 }
